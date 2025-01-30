f1a8c1c8e45da40a2844c8741777f232b06e09b9
BugHunter-X <askzenai@gmail.com>
2025-01-30 05:18:20 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,9 @@
+```javascript
+// Correct usage of Tailwind's utility classes
+
+<div class="bg-gray-100 p-4 text-center">
+  <p>This text is correctly centered.</p>
+</div>
+
+//Note:  No need for @apply here.  Tailwind directives should be used directly within the class attribute.
+```
